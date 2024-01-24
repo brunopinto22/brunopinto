e11a979a73a84c6facdb4d8b4ffd74fc5d14a344
Bruno Pinto <bruno.pinto.bp.20@gmail.com>
2024-01-24 18:47:30 +0000

    feat: ✨ added Components
    main way to get all the Components of the Website

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Hover } from "./Hover/hover";
+
+export { default as Navbar } from "./Navbar/navbar";
+export { default as Hero } from "./Hero/hero";
+export { default as Skills } from "./Skills/skills";
+export { default as Experience } from "./Experience/experience";
+
