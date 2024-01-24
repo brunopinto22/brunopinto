ff8cd5a24514724e26ada81d59bbf67b33046ccd
Bruno Pinto <bruno.pinto.bp.20@gmail.com>
2024-01-24 18:51:21 +0000

    feat: ✨ added Icons

---
+++ src/components/Icons/index.js
@@ -0,0 +1,9 @@
+export { default as Arrow } from "./arrow";
+export { default as Circle } from "./circle";
+export { default as Underline } from "./underline";
+export { default as Corner } from "./corner";
+
+export { default as GitHub } from "./accounts/github";
+export { default as Instagram } from "./accounts/instagram";
+export { default as Linkedin } from "./accounts/linkedin";
+export { default as Mail } from "./accounts/mail";
