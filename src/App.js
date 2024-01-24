9cec7e300e1e240dd18f4edde938a34ffdc98ad3
Bruno Pinto <bruno.pinto.bp.20@gmail.com>
2024-01-24 18:50:50 +0000

    feat: ✨ website base

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import React, { useState, useEffect } from 'react';
+import { MainInfo } from "./data";
+import { Navbar, Hero, Skills, Experience, Hover } from "./components";
 
 function App() {
+
+  document.title = MainInfo.name;
+
+  const [mousePos, setMousePos] = useState({x: 0, y:0});
+  useEffect(() => {
+    const updateMousePos = (e) => { setMousePos({ x: e.clientX, y: e.clientY }); };
+    window.addEventListener('mousemove', updateMousePos);
+
+    return () => { window.removeEventListener('mousemove', updateMousePos); };
+  }, []);
+
+  const [onHover, setOnHover] = useState(false);
+  const [hoverText, setHoverText] = useState('hover here');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <Navbar />
+      
+      <Hero mousePos={mousePos} />
+
+      <Hover mousePos={mousePos} text={hoverText} onHover={onHover} />
+
+      <Skills />
+
+      <Experience setHoverText={setHoverText} setOnHover={setOnHover} />
+
+      <Skills />
+
+    </>
   );
 }
 
