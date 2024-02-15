0e627f9601364946c73b0c4948567e86c52eb2dd
Bruno Pinto <bruno.pinto.bp.20@gmail.com>
2024-02-15 19:12:11 +0000

    feat: ✨ added About Me Section

--- src/components/index.js
+++ src/components/index.js
@@ -7,5 +7,4 @@ export { default as Hero } from "./Hero/hero";
 export { default as Skills } from "./Skills/skills";
 export { default as Experience } from "./Experience/experience";
 export { default as Work } from "./Work/work";
-
-
+export { default as About } from "./About/about";
