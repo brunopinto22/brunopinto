14726cce125493f4f67558939215f39724939e87
Bruno Pinto <bruno.pinto.bp.20@gmail.com>
2024-03-03 22:12:53 +0000

    feat: ✨ added Discord icon

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -3,6 +3,7 @@ export { default as Circle } from "./circle";
 export { default as Underline } from "./underline";
 export { default as Corner } from "./corner";
 
+export { default as Discord } from "./accounts/discord";
 export { default as GitHub } from "./accounts/github";
 export { default as Instagram } from "./accounts/instagram";
 export { default as Linkedin } from "./accounts/linkedin";
