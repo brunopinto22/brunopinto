348cdb0b5dc14b2c4bc5cc5db9c4a419d3cc0b03
Bruno Pinto <bruno.pinto.bp.20@gmail.com>
2024-03-03 23:01:44 +0000

    feat: 🩹 added links

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -25,12 +25,12 @@ function About(props) {
 
         <div className="text">
           <p className='title'>Hi I'm <span className="title-font">{MainInfo.name}</span> , nice to meet you!</p>
-          <p>I'm a <span className="accent">Portuguese</span> Developer that started to get in love with Coding in High School during my professional course where I did an internship at <a href="">Critec</a> and learned more about web development and fell in love with it.</p>
-          <p>After the Course I decided to follow my passion for <span className="accent">Computer Science</span> and pursued Computer Science at <a href="">ISEC</a>.</p> <br />
-          <p>Along the way I made some projects that can be seen in my <a href="">GitHub</a> account.</p> <br />
+          <p>I'm a <span className="accent">Portuguese</span> Developer that started to get in love with Coding in High School during my professional course where I did an internship at <a href="https://www.critec.pt" target='_blank'>Critec</a> and learned more about web development and fell in love with it.</p>
+          <p>After the Course I decided to follow my passion for <span className="accent">Computer Science</span> and pursued Computer Science at <a href="https://www.isec.pt/PT/Default.aspx" target='_blank'>ISEC</a>.</p> <br />
+          <p>Along the way I made some projects that can be seen in my <a href={MainInfo.links.gihub} target='_blank'>GitHub</a> account.</p> <br />
           <p>But enought about my studies and professional life.</p>
           <p>What about my hobbies?</p> <br />
-          <p>I love <span className="accent">drawing</span> and <span className="accent">image manipulation</span>, I have my drawings on my dedicaded Instagram: <a href="">@pintoiguess</a>.</p>
+          <p>I love <span className="accent">drawing</span> and <span className="accent">image manipulation</span>, I have my drawings on my dedicaded Instagram: <a href="https://www.instagram.com/pintoiguess" target='_blank'>@pintoiguess</a>.</p>
         </div>
 
       </div>
